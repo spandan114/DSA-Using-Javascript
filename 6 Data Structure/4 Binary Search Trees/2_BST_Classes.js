1bbde881c12245e2f369f727e7ea6ac994ef4377
spandan joshi <spandanj685@gmail.com>
2020-12-29 17:36:51 +0530

    BST

---
+++ 6 Data Structure/4 Binary Search Trees/2_BST_Classes.js
@@ -0,0 +1,22 @@
+// Every parent node has at most two children
+// Every node to the left of a parent node is always less than the parent
+// Every node to the right of a parent node is always greater than the parent
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+}
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
